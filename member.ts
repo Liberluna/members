58b74de8e46ed63f9b54d093855cda3521f11eee
Shotaro Nakamura <79000684+nakasyou@users.noreply.github.com>
2023-09-17 16:55:02 +0900

    Create member.ts

---
+++ member.ts
@@ -0,0 +1,56 @@
+export interface Member {
+  names: {
+    /**
+     * 英語名
+     */
+    en: string
+    /**
+     * 日本語名
+     */
+    ja: string
+  }
+  /**
+   * 役職
+   */
+  post: {
+    /**
+     * メンバーかどうか。サブメンバーはfalse。
+     */
+    isMember: boolean
+    /**
+     * モデレーターかどうか
+     */
+    isModerator: boolean
+    /**
+     * リーダーかどうか
+     */
+    isLeader: boolean
+  }
+  social: {
+    /**
+     * GitHubのID
+     */
+    github?: string
+    /**
+     * Xのユーザー名
+     */
+    x?: string
+    /**
+     * Scratchのユーザー名
+     */
+    scratch?: string
+    /**
+     * MatrixのID
+     * @example
+     * `@example:example.com`
+     */
+    matrix?: string
+  }
+  /**
+   * 自己紹介、メモ
+   */
+  profile: {
+    ja?: string
+    en?: string
+  }
+}
