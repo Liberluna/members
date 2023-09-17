716297941e5e930c7065609910e88c4f15995e5f
Shotaro Nakamura <79000684+nakasyou@users.noreply.github.com>
2023-09-17 17:31:14 +0900

    Update generate.py

--- scripts/generate.py
+++ scripts/generate.py
@@ -5,11 +5,26 @@ for member_yaml_path in glob.glob('./members/*.yaml'):
     with open(member_yaml_path) as file:
         member_data = yaml.safe_load(file)
         members.append(member_data)
+
 try:
   shutil.rmtree('dist')
 except FileNotFoundError:
     pass
+
 os.mkdir('dist')
 
 with open('dist/members.json', 'w') as file:
-    json.dump(members, file, indent=2)
+    json.dump(members, file, indent=2, ensure_ascii=False)
+
+typescript_code = """
+import data from './members.json' assert { type: 'json' }
+import type { Member } from '../member'
+
+export default data as Member
+""".strip()
+with open('dist/members.ts', 'w') as file:
+    file.write(typescript_code)
+
+python_code = f"""
+members = {json.dumps(members, indent=4)}
+""".strip()
