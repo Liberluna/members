37ae0e613a354dd62e5049e1699abed9a0ca8a3b
Shotaro Nakamura <79000684+nakasyou@users.noreply.github.com>
2023-09-18 14:50:30 +0900

    Update generate_markdown.py

--- scripts/generate_markdown.py
+++ scripts/generate_markdown.py
@@ -1,11 +1,28 @@
 def get_one_member_markdown_by_member_data(member_data) -> str:
+    print(member_data, member_data['post']['isMember'])
     result = f"""
     ### {member_data['names']['ja']}
     #### 名前情報
-    - ja: {member_data['names']['ja']}
-    - en: {member_data['names']['en']}
+    - `ja`: {member_data['names']['ja']}
+    - `en`: {member_data['names']['en']}
     #### ロール情報
     - { '' if {member_data['post']['isMember']} else 'サブ' }メンバー
+    { '- リーダー\n' if member_data['post']['isLeader'] }{ '- モデレーター\n' if member_data['post']['isModerator'] }
+    #### アバター情報
+    
+    <img src=\"{member_data['avatar']}\" width="64" height="64" />
+
+    #### 自己紹介
+    ja:
+    ```
+    { member_data['profile']['ja'] }
+    ```
+    en:
+    ```
+    { member_data['profile']['en'] }
+    ```
+    #### ソーシャル
+    - 
     """
     result = '\n'.join(map(lambda line: line[3:], result[1:-1].split('\n')))
     return result + '\n'
