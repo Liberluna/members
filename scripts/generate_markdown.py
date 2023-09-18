e11b8faf7a3c147494668a4c4a284b6612fe958c
Shotaro Nakamura <79000684+nakasyou@users.noreply.github.com>
2023-09-18 15:43:05 +0900

    Fix syntax error

--- scripts/generate_markdown.py
+++ scripts/generate_markdown.py
@@ -63,7 +63,7 @@ def generate_markdown(data) -> str:
             members.append(one_member_markdown)
         else:
             submembers.append(one_member_markdown)
-    result += '\n## リーダー\' + 'n'.join(leaders)
+    result += '\n## リーダー' + '\n'.join(leaders)
     result += '\n## モデレーター' + '\n'.join(moderators)
     result += '\n## メンバー' + '\n'.join(members)
     result += '\n## サブメンバー' + '\n'.join(submembers)
