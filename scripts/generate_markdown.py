74ef31c6677990fae056e425f6ca96e54d8dc4f9
Shotaro Nakamura <79000684+nakasyou@users.noreply.github.com>
2023-09-18 15:22:54 +0900

    Update generate_markdown.py

--- scripts/generate_markdown.py
+++ scripts/generate_markdown.py
@@ -18,7 +18,7 @@ def get_one_member_markdown_by_member_data(member_data) -> str:
         result += '- モデレーター\n'
     
     result += '#### アバター情報\n'
-    result += '\n\n<img src="' + member_data['avatar'] + '"width="64" height="64" />\n\n'
+    result += '\n\n<img src="' + member_data['avatar'] + '" width="64" height="64" />\n\n'
 
     result += remove_start_by_line(f"""
     #### 自己紹介
@@ -34,13 +34,19 @@ def get_one_member_markdown_by_member_data(member_data) -> str:
     result += '\n'
 
     result += '#### Social\n'
+    if 'matrix' in member_data['social']:
+        result += '- Matrix: [{0}](https://matrix.to/#/{0})\n'.format(member_data['social']['matrix'])
     if 'github' in member_data['social']:
         result += '- GitHub: [{0}](https://github.com/{0})\n'.format(member_data['social']['github'])
-    
+    if 'x' in member_data['social']:
+        result += '- X(Twitter): [@{0}](https://x.com/{0})\n'.format(member_data['social']['x'])
+    if 'scratch' in member_data['social']:
+        result += '- Scratch: [{0}](https://scratch.mit.edu/users/{0})\n'.format(member_data['social']['scratch'])
     return result + '\n'
 def generate_markdown(data) -> str:
     result: str = ''
     result += '# Liberluna Members Markdown ver.\n'
+    result += '⚠️注意 このファイルは自動的に生成されるので、編集しないでください。情報を訂正したい場合は、[members](../members)を編集してください。'
     for member_data in data:
         result += get_one_member_markdown_by_member_data(member_data)
     return result
