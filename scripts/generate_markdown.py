53effbc29da4ca2c42b3ea30a9338c0b617a5f4e
Shotaro Nakamura <79000684+nakasyou@users.noreply.github.com>
2023-09-18 14:38:29 +0900

    Update generate_markdown.py

--- scripts/generate_markdown.py
+++ scripts/generate_markdown.py
@@ -1,2 +1,16 @@
+def get_one_member_markdown_by_member_data(member_data) -> str:
+    result = f"""
+    ### {member_data.names.ja}
+    #### 名前情報
+    - ja: {member_data.names.ja}
+    - en: {member_data.names.en}
+    #### ロール情報
+    - { '' if {member_data.post.isMember} else 'サブ' }メンバー
+    """.strip()
+    return result + '\n'
 def generate_markdown(data) -> str:
-    pass
+    result: str = ''
+    result += '# Liberluna Members Markdown ver.\n'
+    for member_data in data:
+        result += get_one_member_markdown_by_member_data(member_data)
+    return result
